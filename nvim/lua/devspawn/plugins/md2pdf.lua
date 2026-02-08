72543bb02f64bf2ee9cc8b31324f11c5bcc20e21
Tim <timforged@gmail.com>
2026-02-08 12:56:22 +0000

    add rose-pine theme to config

--- nvim/lua/devspawn/plugins/md2pdf.lua
+++ nvim/lua/devspawn/plugins/md2pdf.lua
@@ -2,8 +2,7 @@ return {
 	"alexxGmZ/Md2Pdf",
 	cmd = "Md2Pdf",
 	config = function()
-		local Md2Pdf = require("Md2Pdf")
-		Md2Pdf.setup({
+		require("Md2Pdf").setup({
 			pdf_engine = "pdflatex", -- pdflatex, lualatex, or xelatex
 			yaml_template_path = nil,
 		})
